1193891eba96ecd63c43cc4e37b649cd4d689e94
Bill Chen <Bill.Chen@live.com>
2020-10-15 19:46:15 +0800

    Initial Commit

---
+++ HPMusic/HPMusicApp.swift
@@ -0,0 +1,20 @@
+//
+//  HPMusicApp.swift
+//  HPMusic
+//
+//  Created by Bill Chen on 2020/10/15.
+//
+
+import SwiftUI
+
+@main
+struct HPMusicApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
